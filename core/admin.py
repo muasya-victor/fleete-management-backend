55415f637e05177e150b12b825b3b75499c69d25
DavisWere <devisodhis10@gmail.com>
2024-01-13 21:30:02 +0300

    register user models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from core.models import   User, VehicleService
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Other Fields",
+            {
+                "fields": (
+                    "phone_number",
+                    "company"
+                   
+                )
+            },
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Other Fields",
+            {
+                "fields": (
+                    "phone_number",
+                    "company"
+
+                )
+            },
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+
+admin.site.register(VehicleService)
