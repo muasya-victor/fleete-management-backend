0e6d3a2927568ab5ecf2d921674110d2862f58f3
muasya-victor <vicmwe184@gmail.com>
2024-04-16 22:42:12 +0300

    change db to mysql

--- fleet_management/settings.py
+++ fleet_management/settings.py
@@ -102,12 +102,26 @@ WSGI_APPLICATION = 'fleet_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fleet',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Fleet management Api',
     'DESCRIPTION': 'Fleet management Api',
